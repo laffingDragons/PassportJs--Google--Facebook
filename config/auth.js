42931625ee58234650c2d4630526a6fba1402ea6
laffingragons <akshaypatil,vit@gmail.com>
2018-01-17 14:37:09 +0530

    facebook prob

---
+++ config/auth.js
@@ -0,0 +1,24 @@
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '284309812093395', // your App ID
+        'clientSecret'  : '34beb03d5436b50bf5673822ccca85a7', // your App Secret
+        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback',
+        'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
+        'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:3000/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+    }
+
+};
