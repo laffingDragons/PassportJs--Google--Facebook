a714540692454cea0f3b2b0912e1cdda1881bfd5
laffingragons <akshaypatil,vit@gmail.com>
2018-01-18 00:49:23 +0530

    hppen

--- config/passport.js
+++ config/passport.js
@@ -60,7 +60,7 @@ module.exports = function(passport) {
                     // if there is no user found with that facebook id, create them
                     var newUser            = new User();
 
-                    // set all of the facebook information in our user model
+                     // set all of the facebook information in our user model
                     newUser.facebook.id    = profile.id; // set the users facebook id                   
                     newUser.facebook.token = token; // we will save the token that facebook provides to the user                    
                     newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
